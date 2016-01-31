9bad358b176a22ff3fde5c3da669734d6f047b1b
Lucy Cheng <lcheng@college.harvard.edu>
2016-01-31 17:24:46 -0500

    testing

--- HW1.lua
+++ HW1.lua
@@ -17,13 +17,35 @@ function main()
    local f = hdf5.open(opt.datafile, 'r')
    nclasses = f:read('nclasses'):all():long()[1]
    nfeatures = f:read('nfeatures'):all():long()[1]
+   valid_input = f:read('valid_input'):all()
+   valid_output = f:read('valid_output'):all()
+   train_input = f:read('train_input'):all()
 
-   local W = torch.DoubleTensor(nclasses, nfeatures)
-   local b = torch.DoubleTensor(nclasses)
+   local W = torch.randn(nclasses, nfeatures)
+   local b = torch.randn(nclasses)
 
    -- Train.
+   print(test(W, b, valid_input, valid_output))
 
    -- Test.
 end
+
+function test(W, b, valid_input, valid_output)
+  local size = valid_output:size(1)
+  local output = torch.DoubleTensor(size)
+  local temp = torch.DoubleTensor(#valid_input[1])
+  local num_correct = 0
+  for i = 1, size do
+    truncated = valid_input[{{i,i},{1, valid_input[i]:gt(1):sum()}}][1]
+    temp:add(W:index(2,truncated:long()):sum(2), b)
+    maxval, argmax = temp:max(1)
+    if argmax[1][1] == valid_output[i] then
+      num_correct = num_correct + 1
+    end
+  end
+  return num_correct/size
+end
+    
+
 
 main()
