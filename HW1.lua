058106c077c3616ec9a66ebec9626ff0719d25cc
Lucy Cheng <lcheng@college.harvard.edu>
2016-02-01 12:14:23 -0500

    add comments

--- HW1.lua
+++ HW1.lua
@@ -22,7 +22,7 @@ function main()
    train_input = f:read('train_input'):all()
    train_output = f:read('train_output'):all()
 
-   local W, b = get_naive_bayes(train_input, train_output, 2)
+   local W, b = get_naive_bayes(train_input, train_output, .2)
 
    -- Train.
    print(test(W, b, valid_input, valid_output))
@@ -37,9 +37,11 @@ function test(W, b, input, output)
   local temp = torch.DoubleTensor(#input[1])
   local num_correct = 0
   for i = 1, size do
+    -- gets features --
     truncated = input[{{i,i},{1, input[i]:gt(1):sum()}}][1]
+    -- basically multiplies by feature vector --
     temp:add(W:index(2,truncated:long()):sum(2), b)
-    print(temp)
+    -- gets output class --
     local maxval, argmax = temp:max(1)
     if argmax[1][1] == output[i] then
       num_correct = num_correct + 1
@@ -61,6 +63,7 @@ function get_naive_bayes(input, output, alpha)
 
   local size = input:size(1)
 
+  -- computes F --
   for i = 1, size do
     print(i)
     b[output[i]] = b[output[i]] + 1
@@ -83,6 +86,23 @@ function get_naive_bayes(input, output, alpha)
 
   return W, b
 end
+
+
+--[[
+--in progress--
+function k_fold(k, training_in, training_out, valid_in, valid_out) 
+  local curr = 0
+  local total = 0
+
+  for i = 1, training_out:size(1), training_out:size(1)/k  do
+     
+   local W, b = get_naive_bayes(train_input, train_output, .2)
+
+   -- Train.
+   print(test(W, b, valid_input, valid_output))
+  end
+end
+--]]
 
 
 main()
