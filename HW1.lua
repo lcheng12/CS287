a8ed2576b735bd37c43618d7ea4b5b2d0d20b0db
Lucy Cheng <lcheng@college.harvard.edu>
2016-02-01 00:30:59 -0500

    uhh

--- HW1.lua
+++ HW1.lua
@@ -20,9 +20,9 @@ function main()
    valid_input = f:read('valid_input'):all()
    valid_output = f:read('valid_output'):all()
    train_input = f:read('train_input'):all()
+   train_output = f:read('train_output'):all()
 
-   local W = torch.randn(nclasses, nfeatures)
-   local b = torch.randn(nclasses)
+   local W, b = get_naive_bayes(train_input, train_output, 2)
 
    -- Train.
    print(test(W, b, valid_input, valid_output))
@@ -38,7 +38,8 @@ function test(W, b, valid_input, valid_output)
   for i = 1, size do
     truncated = valid_input[{{i,i},{1, valid_input[i]:gt(1):sum()}}][1]
     temp:add(W:index(2,truncated:long()):sum(2), b)
-    maxval, argmax = temp:max(1)
+    print(temp)
+    local maxval, argmax = temp:max(1)
     if argmax[1][1] == valid_output[i] then
       num_correct = num_correct + 1
     end
@@ -46,6 +47,37 @@ function test(W, b, valid_input, valid_output)
   return num_correct/size
 end
     
+function get_naive_bayes(train_input, train_output, alpha) 
+   local W = torch.DoubleTensor(nclasses, nfeatures)
+   local b = torch.DoubleTensor(nclasses)
+   local F = torch.DoubleTensor(nclasses, nfeatures)
+   F:zero()
+   b:zero()
+
+   local size = train_input:size(1)
+
+   for i = 1, size do
+     print(i)
+     b[train_output[i]] = b[train_output[i]] + 1
+     local curr_class = train_output[i]
+     for j = 1, train_input:size(2) do
+       if train_input[i][j] == 1 then
+         break
+       end
+       F[curr_class][train_input[i][j]] = F[curr_class][train_input[i][j]] + 1 
+     end
+   end
+
+   for i = 1, nclasses do
+     b[i] = math.log(b[i]/size)
+     local s = F:sum(2)[i][1]
+     for j = 1, nfeatures do
+       W[i][j] = math.log((F[i][j] + alpha)/(s + j*alpha))
+     end
+   end
+
+   return W, b
+ end
 
 
 main()
